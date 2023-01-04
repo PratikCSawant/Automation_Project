c96cd49810888808b6693c46510e40b8ab559a6a
PratikCSawant <107945904+PratikCSawant@users.noreply.github.com>
2023-01-04 15:20:23 +0530

    added updated and new files
    added updated automation.sh which has the script for saving the inventory. and added new cron job file which will execute everyday on fixed time.

--- automation.sh
+++ automation.sh
@@ -10,8 +10,8 @@ sudo systemctl status apache2
 
 timeStamp=$(date "+%d%m%Y-%H%M%S")
 
-sudo tar cvf tmp/${userName}-httpd-logs-${timeStamp}.tar /var/log/apache2
+sudo tar cvf /tmp/${userName}-httpd-logs-${timeStamp}.tar /var/log/apache2
 
 aws s3 cp /tmp/${userName}-httpd-logs-${timeStamp}.tar s3://${s3bucketName}/${userName}-httpd-logs-${timeStamp}.tar
 
-
+echo "httpd-logs $(date "+%d%m%Y-%H%M%S") tar     $(stat -c '%s' automation.sh | numfmt --to=si)K"  >> /var/www/html/inventory.html
